0cdce6adf1d38d7d6b576b58359b1707180e9128
Joy Fan <chandler0204@gmail.com>
2023-06-20 17:56:58 +0800

    add db.json

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,14 @@
 import { useTheme, Typography, Box, Paper, Button } from '@mui/material'
 import { notifySuccess, notifyError } from 'utils/notify'
 import useSWR from 'swr'
+
+interface UserSWRType {
+  id: string
+  name: string
+}
 const Login = () => {
   const theme = useTheme()
-  // const { data, error } = useSWR('1', {})
+  const { data, error } = useSWR<UserSWRType>('/user')
   return (
     <Box sx={{ backgroundColor: theme.palette.background.default }}>
       <Paper> Hello</Paper>
@@ -13,6 +18,11 @@ const Login = () => {
       <Typography variant="h2" sx={{ color: theme.palette.primary.darker }}>
         Login
       </Typography>
+      {data && (
+        <Typography variant="h3" sx={{ color: theme.palette.primary.main }}>
+          {data.name}
+        </Typography>
+      )}
       <Button onClick={() => notifySuccess('Success')}>Success</Button>
       <Button onClick={() => notifyError('Error')}>Error</Button>
     </Box>
