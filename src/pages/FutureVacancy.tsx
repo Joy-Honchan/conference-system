c8e2ea77e14641affe1bd966d49404bf7953d43a
Joy Fan <chandler0204@gmail.com>
2023-08-11 11:28:01 +0800

    feat: add DatePicker to future vacancy

--- src/pages/FutureVacancy.tsx
+++ src/pages/FutureVacancy.tsx
@@ -1,5 +1,56 @@
-import React from 'react'
+import { useState } from 'react'
+import {
+  styled,
+  Box,
+  Typography,
+  Stack,
+  Autocomplete,
+  TextField
+} from '@mui/material'
+import { DatePicker } from '@mui/x-date-pickers'
+import { useFormik } from 'formik'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { Button } from '@mui/material'
 
 export default function FutureVacancy() {
-  return <div>FutureVacancy</div>
+  const formik = useFormik({
+    initialValues: {
+      startDate: new Date(),
+      endDate: new Date()
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+  return (
+    <StyleWrapper>
+      <Box>
+        <Typography variant="h4">Search for Room Vacancy</Typography>
+        <form onSubmit={formik.handleSubmit}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Stack spacing={2} direction="row" alignItems={'center'}>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={['One', 'Two', 'Three']}
+                sx={{ width: 300 }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Room No." />
+                )}
+              />
+              <DatePicker label="Start Date" views={['year', 'month', 'day']} />
+              <Typography variant="h4">-</Typography>
+              <DatePicker label="End Date" />
+              <Button variant="contained" type="submit">
+                Submit
+              </Button>
+            </Stack>
+          </LocalizationProvider>
+        </form>
+      </Box>
+    </StyleWrapper>
+  )
 }
+
+const StyleWrapper = styled(Box)(({ theme }) => ({}))
