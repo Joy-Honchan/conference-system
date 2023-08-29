8eb5b28f7ee9760a67dba1f35556c62f5609804a
Joy Fan <chandler0204@gmail.com>
2023-08-29 10:50:22 +0800

    feat: import RoomMap svg

--- src/pages/RoomStatus.tsx
+++ src/pages/RoomStatus.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
-
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import RoomMap from 'images/RoomMap'
 export default function RoomStatus() {
-  return <div>RoomStatus</div>
+  return (
+    <Box>
+      <Typography variant="h4">Room Status</Typography>
+      <RoomMap
+        roomS1={'available'}
+        roomS2={'available'}
+        roomS3={'available'}
+        roomM1={'available'}
+        roomM2={'available'}
+        roomL1={'available'}
+      />
+    </Box>
+  )
 }
