55a1ba038ca635ab12e199ccaa21a9c1a029bfcb
Joy Fan <chandler0204@gmail.com>
2023-08-30 09:53:40 +0800

    feat: add example icon

--- src/pages/RoomStatus.tsx
+++ src/pages/RoomStatus.tsx
@@ -1,26 +1,57 @@
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import RoomMap from 'images/RoomMap'
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline'
 export default function RoomStatus() {
   return (
     <Box
       sx={{
         flexGrow: 1,
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        flexWrap: 'wrap'
       }}
     >
       <Typography variant="body1" sx={{ marginBottom: 1 }}>
         last updated: {new Date().toLocaleString()}
       </Typography>
-      <RoomMap
-        roomS1={'available'}
-        roomS2={'scheduled'}
-        roomS3={'available'}
-        roomM1={'available'}
-        roomM2={'available'}
-        roomL1={'occupied'}
-      />
+      <Box
+        sx={{
+          flexGrow: 1,
+          '#expend-icon': {
+            rect: {
+              transition: 'width 1s ease'
+            },
+            // '#explain_text': {
+            //   transition: 'all 1s ease'
+            // },
+            '&:hover': {
+              rect: {
+                width: 607
+              },
+              '#explain_text': {
+                opacity: 1,
+                transform: 'translateX(0%)'
+              }
+            }
+          },
+          '#explain_text': {
+            opacity: 0,
+            transform: 'translateX(-5%)',
+            transition: 'all 1s ease'
+          }
+        }}
+      >
+        <RoomMap
+          roomS1={'available'}
+          roomS2={'scheduled'}
+          roomS3={'available'}
+          roomM1={'available'}
+          roomM2={'available'}
+          roomL1={'occupied'}
+        />
+      </Box>
     </Box>
   )
 }
